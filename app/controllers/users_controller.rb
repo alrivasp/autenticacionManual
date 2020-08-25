98bfd3aa9aa2e36316714ed5f0c2d90696423206
alrivasp <alvaro.rivas.p@gmail.com>
2020-08-24 21:46:58 -0400

    Correcion de punto en controlador session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save      
-      session[:user_id] = @user.user_id
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path, notice: "Usuario registrado Correctamente"
     else
       render 'new'
